38b6a83b61234581c546d88e3b5ca08027ab6701
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2021-12-11 23:09:13 +0900

    first setting

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Hello World</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    components: {}
   }
 }
 </script>
